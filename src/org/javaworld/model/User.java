8870242f92b0ed07cfcd98748d6d7641607ec825
mongolearner <mongolearner@gmail.com>
2020-05-19 18:55:44 +0530

    Added getter and setter for name variable

--- src/org/javaworld/model/User.java
+++ src/org/javaworld/model/User.java
@@ -3,6 +3,16 @@ package org.javaworld.model;
 public class User {
     
     public String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
     
 
 }
