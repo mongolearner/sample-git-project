059b3feb07a001eb3dc01b5b8e450f055fd9b6d9
mongolearner <mongolearner@gmail.com>
2020-05-20 11:25:17 +0530

    added getter ans setter for age field

--- src/org/javaworld/model/User.java
+++ src/org/javaworld/model/User.java
@@ -5,5 +5,13 @@ public class User {
     public String name;
     public int age;
     
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    
 
 }
